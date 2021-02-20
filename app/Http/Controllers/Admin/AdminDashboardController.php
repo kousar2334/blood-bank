4816f8641745b3ed2dfe2b713764ce0caffeb6ef
Kousar Rahman <kousar.cse2334@egmail.com>
2021-02-20 19:38:20 +0600

    some updates

--- app/Http/Controllers/Admin/AdminDashboardController.php
+++ app/Http/Controllers/Admin/AdminDashboardController.php
@@ -2,10 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
-use Illuminate\Http\Request;
-
 use App\Http\Controllers\Controller;
-use App\User;
 
 class AdminDashboardController extends Controller
 {
--- app/Http/Controllers/Admin/HospitalController.php
+++ app/Http/Controllers/Admin/HospitalController.php
@@ -3,16 +3,17 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use Exception;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class HospitalController extends Controller
 {
-    public function storeHospitalCategory(Request $request){
-        dd($request);
+    public function storeHospitalCategory(Request $request)
+    {
         try{
-        
-         return redirect()->back();
-        }catch(\Exception $e){
+         return redirect()->back()->with($request);
+        }catch(Exception $e){
             return redirect()->back();
         }
     }
